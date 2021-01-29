b18d45cf1ab0f42323ce03a629d226a53973484a
Joan <joanpenya94@gmail.com>
2021-01-29 23:33:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    const container = document.querySelector(".pusheen");
+    container.addEventListener("mousemove", (e) => {
+      const eyes = document.querySelectorAll(".eye");
+      [].forEach.call(eyes, function (eye) {
+        let mouseX = eye.getBoundingClientRect().right;
+        if (eye.classList.contains("eye-left")) {
+          mouseX = eye.getBoundingClientRect().left;
+        }
+        let mouseY = eye.getBoundingClientRect().top;
+        let radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
+        let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
+        eye.style.transform = `rotate(${rotationDegrees}deg)`;
+      });
+    });    
+}
+
